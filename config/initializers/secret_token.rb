29ecd0f7377a580ce59e9defc4d8f12c1947e612
gustavo valenzuela <gustavovalenzuela1180.gmail.com>
2013-10-25 11:15:54 -0700

    first message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fundraisable::Application.config.secret_key_base = 'fe76770d537115b1cd76a5ebbebfc92ba1568ca1f262c44c0b9874dcc4bf1c4439b80d94238cb41c832f5218db9360d698a8a02d91219910a6899ae97021246b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fundraisable::Application.config.session_store :cookie_store, key: '_Fundraisable_session'
